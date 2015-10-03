6cb643421470af83a2a3485cd5c2691eeec4bf20
Chris Fraser <mrfunnel@gmail.com>
2015-10-03 22:57:14 +0200

    Added image

---
+++ www/scripts/controllers/eeArm.js
@@ -0,0 +1,7 @@
+﻿app.controller('eeArm', ['$scope', '$mdSidenav', '$mdBottomSheet', function ($scope, $mdSidenav, $mdBottomSheet) {
+  $scope.openBottomSheet = function () {
+    $mdBottomSheet.show({
+      template: '<md-bottom-sheet>Hello!</md-bottom-sheet>'
+    });
+  };
+}]);
