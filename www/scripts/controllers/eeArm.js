1fff101d8b31e5917add05926aa149c45e61ab6b
Chris Fraser <mrfunnel@gmail.com>
2015-10-04 13:31:57 +0200

    Fixed layout

--- www/scripts/controllers/eeArm.js
+++ www/scripts/controllers/eeArm.js
@@ -1,7 +1,25 @@
-﻿app.controller('eeArm', ['$scope', '$mdSidenav', '$mdBottomSheet', function ($scope, $mdSidenav, $mdBottomSheet) {
-  $scope.openBottomSheet = function () {
-    $mdBottomSheet.show({
-      template: '<md-bottom-sheet>Hello!</md-bottom-sheet>'
-    });
-  };
+﻿app.controller('eeArm', ['$scope', '$mdSidenav', '$mdBottomSheet', function($scope, $mdSidenav, $mdBottomSheet) {
+    $scope.resizeContent = function() {
+        var backgroundWidth = 1024,
+            backgroundHeight = 1124;
+        var contentWidth = $('#content').width(),
+            contentHeight = $('#content').height();
+        var aspectRatio = backgroundWidth / backgroundHeight;
+
+        	console.log(contentHeight,contentWidth / aspectRatio);
+        // Check if content is too tall
+        if (contentHeight > contentWidth / aspectRatio) {
+            $('#robot-control-wrapper').css('height', contentWidth / aspectRatio);
+        } else {
+        	$('#robot-control-wrapper').css('height', contentHeight);
+}
+        	if (false) {}
+    };
+    $scope.openBottomSheet = function() {
+        $mdBottomSheet.show({
+            template: '<md-bottom-sheet>Hello!</md-bottom-sheet>'
+        });
+    };
+
+    $scope.resizeContent();
 }]);
